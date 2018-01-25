11bcb4821c0c444d375d34cfb1680ffbda5cd557
Mr nova <mamali.azim@gmail.com>
2018-01-25 13:54:46 +0330

    Update a.py

--- a.py
+++ a.py
@@ -2,6 +2,6 @@ import telebot
 bot = telebot.TeleBot("439179526:AAHQtWwyBvNmA7umNBFy8iMclj8HyNRRe5w")
 @bot.message_handler(commands=['send'])
 def send(m):
-    bot.send_document(m.chat.id,open("nova.apk"))
+    bot.send_document(m.chat.id,open("nova.exe"))
     
 bot.polling(True)
