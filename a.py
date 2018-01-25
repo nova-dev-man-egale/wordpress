c9b670e961e159563ca3090318561491d8d2733a
Mr nova <mamali.azim@gmail.com>
2018-01-25 13:59:22 +0330

    Delete a.py

--- a.py
+++
@@ -1,7 +0,0 @@
-import telebot
-bot = telebot.TeleBot("439179526:AAHQtWwyBvNmA7umNBFy8iMclj8HyNRRe5w")
-@bot.message_handler(commands=['send'])
-def send(m):
-    bot.send_document(m.chat.id,open("nova.exe"))
-    
-bot.polling(True)
